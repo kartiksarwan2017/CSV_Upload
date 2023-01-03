573d5010e4aa3b1361caf53929c7c67f1f3030db
rohit8450 <kartiksarwan2017.comp@mmcoe.edu.in>
2023-01-03 19:07:48 +0530

    CSV_Upload

--- config/mongoose.js
+++ config/mongoose.js
@@ -5,4 +5,5 @@ const env = require('./environment');
 
 //Set up default mongoose connection
 const mongoDB = env.MongoDB_URL;
+mongoose.set("strictQuery", false);
 module.exports = mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => console.log("CONNECTION ESTABLISHED"));
